42bc7449752cc032d30f2ecc9359000b3b5cd6b4
Júlia Casamitjana Espunya <casamitjana.julia@gmail.com>
2020-07-05 13:10:46 +0200

    updates

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /* ======= Model ======= */
 
 var model = { 
-        currentCat:0, 
+        currentCat: 0,
+        adminMode: false, 
         cats: [
             { name: "Bruc", 
              clickCounter: 0,
@@ -30,9 +31,9 @@ var model = {
 var octopus = {
 
     init: function() {
-
         view_list.render();
         view_info.init(model.cats[0]);
+        view_admin.init();
     },
 
     getList: function(){
@@ -50,35 +51,60 @@ var octopus = {
     count: function(){
         model.cats[model.currentCat].clickCounter++ ;
         view_info.render();
-    } 
+        view_admin.render();
+    },
+
+    getAdminMode: function(){
+        return model.adminMode;
+    },
+
+    admin: function(){
+        model.adminMode = true;
+        view_admin.render();
+    },
+
+    admin_cancel: function(){
+        model.adminMode = false;
+        view_admin.render();
+    }, 
+
+    admin_save: function(a, b, c){
+        model.cats[model.currentCat].name = a;
+        model.cats[model.currentCat].clickCounter = b;
+        model.cats[model.currentCat].image = c;
+
+        view_info.render();
+        view_list.render();
+    }
 
 };
 
-
-
-
+/* ======= View ======= */
 
 var view_list = {
-    
+
     render: function(){
+
     var data = octopus.getList();
+    var list =  document.getElementById("side_list");
+    list.textContent = "";
+
         data.forEach(function(v, i){
         var cat_elem = document.createElement('li');
         cat_elem.textContent = v.name; 
-        document.getElementById("side_list").appendChild(cat_elem);
+        list.appendChild(cat_elem);
         
         cat_elem.addEventListener('click', (function(){
             return function() {
                 octopus.updateCurrentCat(i);
                 view_info.render();
+                view_admin.render();
             }
-        })(cat_elem));
-    });
-
+          })(cat_elem));
+        });
     }
-};
 
-/* ======= View ======= */
+};
 
 
 var view_info = {
@@ -99,13 +125,57 @@ var view_info = {
     render: function(){
         
         var currentCat = octopus.getCurrentCat();
-
+        
         this.picCat.innerHTML = "<img src=img/" + currentCat.image + " >";
         this.nameCat.innerHTML = currentCat.name;
-        this.counterCat.innerHTML = currentCat.clickCounter;  
+        this.counterCat.innerHTML = currentCat.clickCounter; 
+
 
     }
         
+};
+
+var view_admin = {
+    init: function(){
+        this.adminButton = document.getElementById("admin_button");
+        this.adminButton_cancel = document.getElementById("cancel");
+        this.adminButton_save = document.getElementById("save");
+
+        this.viewAdmin = document.getElementById("admin");
+        this.nameAdmin = document.getElementById("name");
+        this.clicksAdmin = document.getElementById("clicks");
+        this.fotoAdmin = document.getElementById("foto");
+
+        this.adminButton.addEventListener('click', (function() {
+            octopus.admin();
+        })); 
+
+        this.adminButton_cancel.addEventListener('click', (function() {
+            octopus.admin_cancel();
+        })); 
+
+        this.adminButton_save.addEventListener('click', (function() {
+            var newName = document.getElementById("name").value;
+            var newClicks = document.getElementById("clicks").value;
+            var newFoto = document.getElementById("foto").value;
+            console.log(newName);
+            octopus.admin_save( newName, newClicks, newFoto);
+        })); 
+
+    },
+
+    render: function(){
+
+        var adminMode = octopus.getAdminMode();
+        var currentCat = octopus.getCurrentCat();
+
+        adminMode ? this.viewAdmin.style.display = "block" : this.viewAdmin.style.display = "none";
+
+        this.nameAdmin.value = currentCat.name;
+        this.clicksAdmin.value = currentCat.clickCounter;
+        this.fotoAdmin.value = currentCat.image;
+
+    }
 
 };
 
